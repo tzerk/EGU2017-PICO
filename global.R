9b692d42e423d5caf8eca069d21fa54b0d34ce5f
tzerk <XnIASwkTCBlGgudcCsml>
2017-04-18 12:28:34 +0200

    add About modal (follow-up commit)
    ...should have been committed with the previous one

--- global.R
+++ global.R
@@ -5,6 +5,24 @@ library(highlight)
 library(ggplot2)
 source("snippets/package_stats.R")
 
+## Header
+about_text <- paste("This presentation was entirely written in <b>R</b> using the",
+                    tags$code("shiny"), "framework. Everything you see in this presentation",
+                    "is dynamically rendered by <b>R</b> or, more specifically, by",
+                    tags$code("Shiny Server"), "running on a server hosted at the Institute of Geography at",
+                    "the University of Cologne.", tags$br(), tags$br(),
+                    "If you want to have a look at, download or re-use the <b>R</b> code",
+                    "used to produce this web application please see the 'Further resources' section",
+                    "on the side bar and scan the corresponding QR code. Note that for this PICO presentation",
+                    "hyperlinks were deliberately removed to prevent undesired context switches",
+                    "and thus to retain its integrity during the conference. If you are watching this presentation",
+                    "on a mobile device please open the following URL in a desktop browser", tags$br(), tags$br(),
+                    tags$a(href = "#", "http://rlum.geographie.uni-koeln.de:3838/EGU2017-PICO/"), tags$br(), tags$br(),
+                    "in order to be able to scan the QR codes. If you want to contact the author",
+                    "of this presentation you can send an email to", tags$a(href = "#", "christoph.burow@uni-koeln.de"),
+                    ".") %>% 
+  HTML()
+
 ## Presentation abstract
 title <- HTML(paste("Easing access to R using 'shiny' to create graphical user interfaces:<br/>",
                     " An example for the R package 'Luminescence'"))
